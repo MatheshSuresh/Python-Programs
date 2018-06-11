69c4d89eca589980403b1e45d07a433147df018a
Mathesh Suresh <33441626+MatheshSuresh@users.noreply.github.com>
2018-06-11 14:10:36 +0530

    Add files via upload

---
+++ celcius_to_fahrenheit.py
@@ -0,0 +1,3 @@
+celsius=int(input("Enter the Celsius : "))
+fahrenheit=celsius*33.8
+print("Fahrenheit",fahrenheit)
---
+++ kilometer_to_miles.py
@@ -0,0 +1,3 @@
+kilometer=int(input("Enter the Kilometer :"))
+miles=kilometer*0.621371
+print (miles,"Miles")
