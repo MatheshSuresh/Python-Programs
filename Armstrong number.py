4ced4ba4f7c92fb4e9a05d8c3763fbc06c61c674
Mathesh Suresh <33441626+MatheshSuresh@users.noreply.github.com>
2018-06-14 10:35:23 +0530

    Update Armstrong number.py

--- Armstrong number.py
+++ Armstrong number.py
@@ -1,13 +1,14 @@
-def prime_number(n):
-    value=0
-    for i in range(2,int(n/2)):
-        if(n%i==0):
-            value=1
-
-    if(value==1):
-        print("It not a is prime number")
-    else:
-        print("It is a prime number")
-
-x=int(input("Enter the number : "))
-prime_number(x)
+num = int(input("enter a number: "))
+ 
+length = len(str(num))
+sum = 0
+temp = num
+ 
+while(temp != 0):
+    sum = sum + ((temp % 10) ** length)
+    temp = temp // 10
+ 
+if sum == num:
+    print("armstrong number")
+else:
+    print("not armstrong number")
